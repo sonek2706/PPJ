6aa9fee9cde9a9d606d8ab4fb22f26fb8b01a3df
Jan Ha <s26092@pjwstk.edu.pl>
2021-11-04 13:56:55 +0100

    Task0708

--- Tasks/src/Task0708.java
+++ Tasks/src/Task0708.java
@@ -2,19 +2,21 @@ import java.util.Scanner;
 
 public class Task0708 {
 
-    public static boolean inSum(){
-        boolean result = false;
-
-        return result;
+    public static boolean inSum(int a, int b, int c, int d, int n){
+        if ((n >= a && n <= b) || (n >= c && c <= d))
+            return true;
+        else
+            return false;
     }
 
-    public static boolean inIntersect(){
-        boolean result = false;
-
-        return result;
+    public static boolean inIntersect(int a, int b, int c, int d, int n){
+        if (n >= )
+            return true;
+        else
+            return false;
     }
 
-    public static boolean inSymDiff(){
+    public static boolean inSymDiff(int a, int b, int c, int d, int n){
         boolean result = false;
 
         return result;
@@ -23,7 +25,7 @@ public class Task0708 {
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
         int a, b, c ,d;
-        System.out.println("Please enter your input");
+        System.out.println("Please enter your input, so that a < b, c < d and a,b,c,d belongs to [-5;5].");
         System.out.print("a = ");
         a = sc.nextInt();
         System.out.print("b = ");
@@ -33,6 +35,10 @@ public class Task0708 {
         System.out.print("d = ");
         d = sc.nextInt();
 
+        for (int n = -5; n <= 5; n++){
+            System.out.printf("%2d",n);
+            System.out.println(":inSum? " + inSum(a,b,c,d, n) + ";inIntersect?"+inIntersect(a,b,c,d, n)+";inSymDiff?"+inSymDiff(a,b,c,d, n));
+        }
 
 
     }
