67e5740e8579d4b4c3191606cf2ba0d970e63729
Jan Ha <s26092@pjwstk.edu.pl>
2021-11-04 12:50:34 +0100

    Task0705

---
+++ Tasks/src/Task0705.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Task0705 {
+
+    public static int sumOfDigits(int n) {
+        int sum = 0;
+        while (n>0){
+            sum += n%10;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n < 0){
+            System.out.println("Your input is wrong, try sth else!");
+        }
+
+        int max = 0, max_val = 0;
+        while (n!=0){
+            int tmp = sumOfDigits(n);
+            if (tmp > max_val){
+                max_val = tmp;
+                max = n;
+            }
+            n = sc.nextInt();
+        }
+
+        System.out.println("Max sum of digits was "+ max_val +" for " + max);
+    }
+}
