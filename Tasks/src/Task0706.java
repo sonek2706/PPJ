198edd4cfc8a1eca87fbd8641b707edec086ec97
Jan Ha <s26092@pjwstk.edu.pl>
2021-11-04 13:16:01 +0100

    Task0706

---
+++ Tasks/src/Task0706.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Task0706 {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println("Please enter your input: ");
+        n = sc.nextInt();
+        if (n <= 0){
+            System.out.println("Wrong input! Error");
+        }
+        else {
+            for (int i = 1; i <= n; i++){
+                for (int j = 1; j <= n; j++) {
+                    System.out.printf("%4d",i*j);
+                }
+                System.out.print("\n");
+            }
+        }
+    }
+}
